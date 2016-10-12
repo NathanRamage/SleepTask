5f3e483bd8d2e0ebb6d15b6953d2035d465d455f
NathanRamage <nramage94@gmail.com>
2016-10-13 00:24:49 +1000

    Sleep task ver 0.1
    Features:
    Add new task
    
    Backend:
    Create database
    Create table
    Add task to table
    
    TODO:
    Update list view to show added tasks
    Function to delete tasks from database using 'Done' Button

---
+++ SleepTask/app/src/main/java/com/example/nathan/sleeptask/db/TaskContract.java
@@ -0,0 +1,18 @@
+package com.example.nathan.sleeptask.db;
+
+/**
+ * Created by nathan on 12/10/16.
+ */
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.nathan.sleeptask.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
